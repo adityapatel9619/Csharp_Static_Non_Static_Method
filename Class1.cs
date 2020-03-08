4fad8ff008d7570150a349bcc2fc1b9167383238
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-03-08 10:04:02 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Static_Non_Static_Method
+{
+    class Class1
+    {
+        /* 
+         Now we will declare method in this class and we will call from Program.cs 
+         */
+              
+        
+        //Non-Static Method
+        public static void name2()
+        {
+            Console.WriteLine("I am Non-Static Class From  Class1");
+        }
+
+        //Static Method
+        public void name3()
+        {
+            Console.WriteLine("I am Static Class From  Class1");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Static_Non_Static_Method
+{
+    class Program
+    {
+
+    // **Refernce Type Method**
+        //Static & Non-Static Method Declaration
+            
+            //Static Method
+        static void name()
+        {
+            Console.WriteLine("I am from Static Class");
+        }
+
+            //Non-Static Method
+
+        public void name1()
+        {
+            Console.WriteLine("I am from Non-Static Class");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Program.cs********");
+            //Calling Static Method 
+            name();
+
+            //Calling Non-Static Method
+            //To call non-static method we have to make instance and and call the non-static method
+
+            Program m = new Program();
+            m.name1();
+
+            Console.WriteLine("********Class1.cs********");
+            /*Here we will call methods from Class1.cs*/
+
+            //Calling Non-Static Method from Class1.cs
+            Class1.name2();
+
+            //Calling Static Method from Class1.cs
+            Class1 c = new Class1();
+            c.name3();
+            
+            
+        }
+    }
+}
